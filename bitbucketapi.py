e4dff49a565d9ae94f5eab9a32b060126df39103
ashokkumar2706 <48880396+ashokkumar2706@users.noreply.github.com>
2021-08-13 09:15:48 +0530

    Update bitbucketapi.py

--- bitbucketapi.py
+++ bitbucketapi.py
@@ -2,8 +2,8 @@ import requests
 from requests.auth import HTTPBasicAuth
 
 ##Login
-username = 'ashok2706'
-password = 'Password01!'
+username = 'xxx'
+password = 'xxx'
 #team = 'xxx'
 
 full_repo_list = []
@@ -15,6 +15,14 @@ next_page_url = 'https://api.bitbucket.org/2.0/repositories/ashok2706/testrepo?f
 while next_page_url is not None:
   response = requests.get(next_page_url, auth=(username, password))
   page_json = response.json()
+
+  # Parse repositories from the JSON
+ # for repo in page_json['values']:
+ #   full_repo_list.append(repo['slug'])
+
+  # Get the next page URL, if present
+  # It will include same query parameters, so no need to append them again
+  next_page_url = page_json.get('next', None)
 
 # Result length will be equal to `size` returned on any page
 print ("Result:", page_json)
